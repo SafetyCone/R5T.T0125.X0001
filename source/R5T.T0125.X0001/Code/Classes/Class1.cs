29ef372806ba12dbf5b39a2a4761041a137fc804
davidcoats <david@getsafetycone.com>
2022-01-28 12:39:05 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0125.X0001/Code/Classes/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+namespace R5T.T0125.X0001
+{
+    public class Class1
+    {
+    }
+}
---
+++ source/R5T.T0125.X0001/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0125.X0001
+{
+    /// <summary>
+    /// Rivet specific UsingDirectivesSpecification and related types, extension method bases, and extensions.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
